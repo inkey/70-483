e7d69a8b29073354a529a8e5da3ecb289e61487e
inkey <phelimmbrady@gmail.com>
2017-10-25 22:01:57 +0100

    Add project files.

---
+++ 70-483/CreatingCustomStruct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70_483
+{
+    struct CreatingCustomStruct
+    {
+        public int x, y;
+
+        public CreatingCustomStruct(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+}
