58c043e55f8a9cdd1b2a0408459420b956dbc2e1
inkey <phelimmbrady@gmail.com>
2017-10-30 22:02:25 +0000

    commit all projects

---
+++ ManageProgramFlow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task Parallel Libary
+            /*
+             * ParallelFor
+             * PLINQ
+             * Tasks
+             */
+
+            //Async/Await keywords
+
+            //Concurrent Collections
+            /*
+             ConcurrentBag
+             ConcurrentDictionary
+             ConcurrentQueue
+             BlockingCollection 
+             */
+        }
+    }
+}
